a62d3337f24591dd1c6b30288f51381cb4a98b57
nanangqq <gingggg@gmail.com>
2021-09-15 10:51:35 +0900

    chapter4 복습

--- src/main/java/com/example/study/model/entity/Item.java
+++ src/main/java/com/example/study/model/entity/Item.java
@@ -19,6 +19,18 @@ public class Item {
     private Integer price;
     private String content;
 
+    // 1:N
+    // LAZY = 지연로딩, EAGER = 즉시로딩
+
+    // LAZY -> select * from item where id = ?
+
+    // EAGER -> 1:1 에서만 기본적으로 사용하라,,,
+    // item_id = order_detail.item_id
+    // user_id = order_detail.user_id
+    // where item.id = ?
+    // 연관관계 설정된 모든 테이블에 대해서 조인 후 데이터를 가져옴
+    // 데이터가 많은 테이블에 대해서 EAGER로 걸려있으면 성능저하, 데이터 많아서 못가져오는 경우 등이 생길 수 있음
+    // 1:N 같이 여러가지 데이터 로딩이 일어날 수 있는 경우에는 반드시 LAZY 로 걸어줘야 함.
     @OneToMany(fetch = FetchType.LAZY, mappedBy = "item")
     private List<OrderDetail> orderDetailList;
 }
--- src/main/java/com/example/study/repository/UserRepository.java
+++ src/main/java/com/example/study/repository/UserRepository.java
@@ -5,7 +5,18 @@ import com.example.study.model.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    // select * from user where account = ? << test03, test04 ... account 로 검색
+    Optional<User> findByAccount(String account);
+
+    Optional<User> findByEmail(String email);
+
+    // select * from user where account = ? and email = ? << account, email 로 검색
+    // 변수명, 컬럼명을 꼭 맞추지 않아도 되지만,, 순서대로 매칭됨. 하지만 맞춰주는것 추천함.
+    Optional<User> findByAccountAndEmail(String account, String email);
 
 }
